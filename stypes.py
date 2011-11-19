10a6ad9053f9b1a1b82ecdb5c146b74e0a178ca6
pjhades <pj.hades@gmail.com>
2011-11-19 12:49:04 +0800

    simplified the design of type operations

--- stypes.py
+++ stypes.py
@@ -3,40 +3,22 @@
 import utils
 from errors import *
 
-# Additions
-def _add_complex_complex(a, b):
-    new_real = _op_table['add'][a.real.tag + b.real.tag](a.real, b.real)
-    new_imag = _op_table['add'][a.imag.tag + b.imag.tag](a.imag, b.imag)
+_COMPLEX = 4
+_REAL = 2
+_RATIONAL = 1
 
+# Additions
+def _add_complex(a, b):
+    new_real = a.real + b.real
+    new_imag = a.imag + b.imag
     if Boolean.true(new_imag == Rational(0, 1)):
         return new_real
-
     return Complex(new_real, new_imag)
 
-def _add_complex_real(a, b):
-    if a.tag == 'Real':
-        a, b = b, a
-    return a + Complex(b, Real(0.0))
-
-def _add_complex_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-    return a + Complex(b, Rational(0, 1))
-
-    return Complex(op(a.real, b), a.imag)
-
-def _add_real_real(a, b):
+def _add_real(a, b):
     return Real(a.value + b.value)
 
-def _add_real_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-
-    r = Real(b.numer / b.denom)
-
-    return Real(a.value + r.value)
-
-def _add_rational_rational(a, b):
+def _add_rational(a, b):
     new_numer = a.numer * b.denom + b.numer * a.denom
     new_denom = a.denom * b.denom
     gcd = Rational._gcd(new_numer, new_denom)
@@ -44,44 +26,21 @@ def _add_rational_rational(a, b):
     new_denom //= gcd
     if new_denom < 0:
         new_numer, new_denom = -new_numer, -new_denom
-
     return Rational(new_numer, new_denom)
 
 
 # Subtractions
-def _sub_complex_complex(a, b):
-    new_real = _op_table['sub'][a.real.tag + b.real.tag](a.real, b.real)
-    new_imag = _op_table['sub'][a.imag.tag + b.imag.tag](a.imag, b.imag)
-
+def _sub_complex(a, b):
+    new_real = a.real - b.real
+    new_imag = a.imag - b.imag
     if Boolean.true(new_imag == Rational(0, 1)):
         return new_real
-
     return Complex(new_real, new_imag)
 
-def _sub_complex_real(a, b):
-    return a - Complex(b, Real(0.0))
-
-def _sub_real_complex(a, b):
-    return Complex(a, Real(0.0)) - b
-
-def _sub_complex_rational(a, b):
-    return a - Complex(b, Rational(0, 1))
-
-def _sub_rational_complex(a, b):
-    return Complex(a, Rational(0, 1)) - b
-
-def _sub_real_real(a, b):
+def _sub_real(a, b):
     return Real(a.value - b.value)
 
-def _sub_real_rational(a, b):
-    r = Real(b.numer / b.denom)
-    return Real(a.value - r.value)
-
-def _sub_rational_real(a, b):
-    r = Real(a.numer / a.denom)
-    return Real(r.value - b.value)
-
-def _sub_rational_rational(a, b):
+def _sub_rational(a, b):
     new_numer = a.numer * b.denom - b.numer * a.denom
     new_denom = a.denom * b.denom
     gcd = Rational._gcd(new_numer, new_denom)
@@ -89,56 +48,20 @@ def _sub_rational_rational(a, b):
     new_denom //= gcd
     if new_denom < 0:
         new_numer, new_denom = -new_numer, -new_denom
-
     return Rational(new_numer, new_denom)
 
 # Multiplications
-def _mul_complex_complex(a, b):
-    op_rr = _op_table['mul'][a.real.tag + b.real.tag]
-    op_ri = _op_table['mul'][a.real.tag + b.imag.tag]
-    op_ir = _op_table['mul'][a.imag.tag + b.real.tag]
-    op_ii = _op_table['mul'][a.imag.tag + b.imag.tag]
-
-    new_real = op_rr(a.real, b.real) + op_ii(a.imag, b.imag)
-    new_imag = op_ri(a.real, b.imag) + op_ir(a.imag, b.real)
-
+def _mul_complex(a, b):
+    new_real = a.real * b.real - a.imag * b.imag
+    new_imag = a.real * b.imag + a.imag * b.real
     if Boolean.true(new_imag == Rational(0, 1)):
         return new_real
     return Complex(new_real, new_imag)
 
-def _mul_complex_real(a, b):
-    if a.tag == 'Real':
-        a, b = b, a
-
-    if a.real.tag != 'Real':
-        a.real = Real(a.real.numer / a.real.denom)
-        a.imag = Real(a.imag.numer / a.imag.denom)
-
-    op = _op_table['mul']['RealReal']
-
-    return Complex(op(a.real, b), op(a.imag, b))
-
-def _mul_complex_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-
-    if a.real.tag == 'Real':
-        b = Real(b.numer / b.denom)
-
-    op = _op_table['mul'][a.real.tag + b.tag]
-
-    return Complex(op(a.real, b), op(a.imag, b))
-
-def _mul_real_real(a, b):
+def _mul_real(a, b):
     return Real(a.value * b.value)
 
-def _mul_real_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-    r = Real(b.numer / b.denom)
-    return Real(a.value * r.value)
-
-def _mul_rational_rational(a, b):
+def _mul_rational(a, b):
     new_numer = a.numer * b.numer
     new_denom = a.denom * b.denom
     gcd = Rational._gcd(new_numer, new_denom)
@@ -146,61 +69,18 @@ def _mul_rational_rational(a, b):
     new_denom //= gcd
     if new_denom < 0:
         new_numer, new_denom = -new_numer, -new_denom
-
     return Rational(new_numer, new_denom)
 
 # Divisions
-def _div_complex_complex(a, b):
+def _div_complex(a, b):
     factor = b.real * b.real + b.imag * b.imag
+    c = a * b._conjugate()
+    return Complex(c.real / factor, c.imag / factor)
 
-    return a * b._conjugate() / factor
-
-def _div_complex_real(a, b):
-    if a.real.tag != 'Real':
-        a.real = Real(a.real.numer / a.real.denom)
-        a.imag = Real(a.imag.numer / a.imag.denom)
-
-    op = _op_table['div']['RealReal']
-
-    return Complex(op(a.real, b), op(a.imag, b))
-
-def _div_real_complex(a, b):
-    if b.real.tag != 'Real':
-        b.real = Real(b.real.numer / b.real.denom)
-        b.imag = Real(b.imag.numer / b.imag.denom)
-
-    factor = b.real * b.real + b.imag * b.imag
-
-    return a * b._conjugate() / factor
-
-def _div_complex_rational(a, b):
-    if a.real.tag == 'Real':
-        b = Real(b.numer / b.denom)
-
-    op = _op_table['div'][a.real.tag + b.tag]
-
-    return Complex(op(a.real, b), op(a.imag, b))
-
-def _div_rational_complex(a, b):
-    if b.real.tag == 'Real':
-        a = Real(a.numer / a.denom)
-
-    factor = b.real * b.real + b.imag * b.imag
-
-    return a * b._conjugate() / factor
-
-def _div_real_real(a, b):
+def _div_real(a, b):
     return Real(a.value / b.value)
 
-def _div_real_rational(a, b):
-    r = Real(b.numer / b.denom)
-    return Real(a.value / r.value)
-
-def _div_rational_real(a, b):
-    r = Real(a.numer / a.denom)
-    return Real(r.value / b.value)
-
-def _div_rational_rational(a, b):
+def _div_rational(a, b):
     new_numer = a.numer * b.denom
     new_denom = a.denom * b.numer
     gcd = Rational._gcd(new_numer, new_denom)
@@ -208,255 +88,176 @@ def _div_rational_rational(a, b):
     new_denom //= gcd
     if new_denom < 0:
         new_numer, new_denom = -new_numer, -new_denom
-
     return Rational(new_numer, new_denom)
 
 
 # Comparisons
-def _eq_complex_complex(a, b):
+def _eq_complex(a, b):
     return (a.real == b.real) * (a.imag == b.imag)
 
-def _eq_complex_real(a, b):
-    return Boolean(False)
-
-def _eq_complex_rational(a, b):
-    return Boolean(False)
-
-def _ne_complex_complex(a, b):
+def _ne_complex(a, b):
     return (a.real != b.real) + (a.imag != b.imag)
 
 
-def _lt_real_real(a, b):
+def _lt_real(a, b):
     return Boolean(a.value < b.value)
 
-def _le_real_real(a, b):
+def _le_real(a, b):
     return Boolean(a.value <= b.value)
 
-def _gt_real_real(a, b):
+def _gt_real(a, b):
     return Boolean(a.value > b.value)
 
-def _ge_real_real(a, b):
+def _ge_real(a, b):
     return Boolean(a.value >= b.value)
 
-def _eq_real_real(a, b):
+def _eq_real(a, b):
     return Boolean(a.value == b.value)
 
-def _ne_real_real(a, b):
+def _ne_real(a, b):
     return Boolean(a.value != b.value)
 
 
-def _lt_real_rational(a, b):
-    return Boolean(a.value < b.numer / b.denom)
-
-def _lt_rational_real(a, b):
-    return Boolean(a.numer / a.denom < b.value)
-
-def _le_real_rational(a, b):
-    return Boolean(a.value <= b.numer / b.denom)
-
-def _le_rational_real(a, b):
-    return Boolean(a.numer / a.denom <= b.value)
-
-def _gt_real_rational(a, b):
-    return Boolean(a.value > b.numer / b.denom)
-
-def _gt_rational_real(a, b):
-    return Boolean(a.numer / a.denom > b.value)
-
-def _ge_real_rational(a, b):
-    return Boolean(a.value <= b.numer / b.denom)
-
-def _ge_rational_real(a, b):
-    return Boolean(a.numer / a.denom <= b.value)
-
-def _eq_real_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-    return Boolean(a.value == b.numer / b.denom)
-
-def _ne_real_rational(a, b):
-    if a.tag == 'Rational':
-        a, b = b, a
-    return Boolean(a.value != b.numer / b.denom)
-
-
-def _lt_rational_rational(a, b):
+def _lt_rational(a, b):
     return Boolean(a.numer * b.denom < a.denom * b.numer)
 
-def _le_rational_rational(a, b):
+def _le_rational(a, b):
     return Boolean(a.numer * b.denom <= a.denom * b.numer)
 
-def _gt_rational_rational(a, b):
+def _gt_rational(a, b):
     return Boolean(a.numer * b.denom > a.denom * b.numer)
 
-def _ge_rational_rational(a, b):
+def _ge_rational(a, b):
     return Boolean(a.numer * b.denom >= a.denom * b.numer)
 
-def _eq_rational_rational(a, b):
+def _eq_rational(a, b):
     return Boolean(a.numer * b.denom == a.denom * b.numer)
 
-def _ne_rational_rational(a, b):
+def _ne_rational(a, b):
     return Boolean(a.numer * b.denom != a.denom * b.numer)
 
 
-_op_table = {'add': {'ComplexComplex': _add_complex_complex, \
-                     'ComplexReal': _add_complex_real, \
-                     'RealComplex': _add_complex_real, \
-                     'ComplexRational': _add_complex_rational, \
-                     'RationalComplex': _add_complex_rational, \
-                     'RealReal': _add_real_real, \
-                     'RealRational': _add_real_rational, \
-                     'RationalReal': _add_real_rational, \
-                     'RationalRational': _add_rational_rational
-                    }, \
-             'sub': {'ComplexComplex': _sub_complex_complex, \
-                     'ComplexReal': _sub_complex_real, \
-                     'RealComplex': _sub_real_complex, \
-                     'ComplexRational': _sub_complex_rational, \
-                     'RationalComplex': _sub_rational_complex, \
-                     'RealReal': _sub_real_real, \
-                     'RealRational': _sub_real_rational, \
-                     'RationalReal': _sub_rational_real, \
-                     'RationalRational': _sub_rational_rational
-                    }, \
-             'mul': {'ComplexComplex': _mul_complex_complex, \
-                     'ComplexReal': _mul_complex_real, \
-                     'RealComplex': _mul_complex_real, \
-                     'ComplexRational': _mul_complex_rational, \
-                     'RationalComplex': _mul_complex_rational, \
-                     'RealReal': _mul_real_real, \
-                     'RealRational': _mul_real_rational, \
-                     'RationalReal': _mul_real_rational, \
-                     'RationalRational': _mul_rational_rational
-                    }, \
-             'div': {'ComplexComplex': _div_complex_complex, \
-                     'ComplexReal': _div_complex_real, \
-                     'RealComplex': _div_real_complex, \
-                     'ComplexRational': _div_complex_rational, \
-                     'RationalComplex': _div_rational_complex, \
-                     'RealReal': _div_real_real, \
-                     'RealRational': _div_real_rational, \
-                     'RationalReal': _div_rational_real, \
-                     'RationalRational': _div_rational_rational 
-                    }, \
-             'lt': {'RealReal': _lt_real_real, \
-                    'RealRational': _lt_real_rational, \
-                    'RationalReal': _lt_rational_real, \
-                    'RationalRational': _lt_rational_rational
-                   }, \
-             'le': {'RealReal': _le_real_real, \
-                    'RealRational': _le_real_rational, \
-                    'RationalReal': _le_rational_real, \
-                    'RationalRational': _le_rational_rational
-                   }, \
-             'gt': {'RealReal': _gt_real_real, \
-                    'RealRational': _gt_real_rational, \
-                    'RationalReal': _gt_rational_real, \
-                    'RationalRational': _gt_rational_rational
-                   }, \
-             'ge': {'RealReal': _ge_real_real, \
-                    'RealRational': _ge_real_rational, \
-                    'RationalReal': _ge_rational_real, \
-                    'RationalRational': _ge_rational_rational
-                   }, \
-             'eq': {'ComplexComplex': _eq_complex_complex, \
-                    'ComplexReal': _eq_complex_real, \
-                    'RealComplex': _eq_complex_real, \
-                    'ComplexRational': _eq_complex_rational, \
-                    'RationalComplex': _eq_complex_rational, \
-                    'RealReal': _eq_real_real, \
-                    'RealRational': _eq_real_rational, \
-                    'RationalReal': _eq_real_rational, \
-                    'RationalRational': _eq_rational_rational
-                   }, \
-             'ne': {'ComplexComplex': _ne_complex_complex, \
-                    'RealReal': _ne_real_real, \
-                    'RealRational': _ne_real_rational, \
-                    'RationalReal': _ne_real_rational, \
-                    'RationalRational': _ne_rational_rational
-                   } \
-           }
+def _rational_to_complex(a):
+    return Complex(a, Rational(0, 1))
 
+def _rational_to_real(a):
+    return Real(a.numer / a.denom)
 
-# Call this in each type
-# add this layer to separate the modification on the table
-def check_num(f):
-    """Check if arguments are numbers"""
+def _real_to_complex(a):
+    return Complex(a, Real(0.0))
+
+_op_table = {'+': {_COMPLEX: _add_complex, \
+                   _REAL: _add_real, \
+                   _RATIONAL: _add_rational}, \
+
+             '-': {_COMPLEX: _sub_complex, \
+                   _REAL: _sub_real, \
+                   _RATIONAL: _sub_rational}, \
+
+             '*': {_COMPLEX: _mul_complex, \
+                   _REAL: _mul_real, \
+                   _RATIONAL: _mul_rational}, \
+             
+             '/': {_COMPLEX: _div_complex, \
+                   _REAL: _div_real, \
+                   _RATIONAL: _div_rational}, \
+              
+             '>': {_REAL: _gt_real, \
+                   _RATIONAL: _gt_rational}, \
+
+             '>=': {_REAL: _ge_real, \
+                    _RATIONAL: _ge_rational}, \
+                   
+             '<': {_REAL: _lt_real, \
+                   _RATIONAL: _lt_rational}, \
+
+             '<=': {_REAL: _le_real, \
+                    _RATIONAL: _lt_rational}, \
+
+             '==': {_COMPLEX: _eq_complex, \
+                    _REAL: _eq_real, \
+                    _RATIONAL: _eq_rational}, \
+
+             '!=': {_COMPLEX: _ne_complex, \
+                    _REAL: _ne_real, \
+                    _RATIONAL: _ne_rational}, \
+
+             '->': {_RATIONAL + _COMPLEX: _rational_to_complex, \
+                    _RATIONAL + _REAL: _rational_to_real, \
+                    _REAL + _COMPLEX: _real_to_complex}
+            }
+
+def _convert(orig, target):
+    return _op_table['->'][orig.tag + target.tag](orig)
+
+def check_operand(f):
     def func(a, b):
         try:
-            return f(a, b)
+            if a.tag > b.tag:
+                return f(a, _convert(b, a))
+            elif a.tag < b.tag:
+                return f(_convert(a, b), b)
+            else:
+                return f(a, b)
         except AttributeError:
             raise SchemeTypeError('', 'expects numbers as arguments')
-    return func
-
-def check_permit(f):
-    """Check if operation is permitted"""
-    def func(a, b):
-        try:
-            return f(a, b)
         except KeyError:
             raise SchemeTypeError('', 'operation is not supported')
     return func
 
-@check_num
+
+@check_operand
 def _add(a, b):
-    return _op_table['add'][a.tag + b.tag](a, b)
+    return _op_table['+'][a.tag](a, b)
 
-@check_num
+@check_operand
 def _sub(a, b):
-    return _op_table['sub'][a.tag + b.tag](a, b)
+    return _op_table['-'][a.tag](a, b)
 
-@check_num
+@check_operand
 def _mul(a, b):
-    return _op_table['mul'][a.tag + b.tag](a, b)
+    return _op_table['*'][a.tag](a, b)
 
-@check_num
+@check_operand
 def _div(a, b):
-    return _op_table['div'][a.tag + b.tag](a, b)
+    return _op_table['/'][a.tag](a, b)
 
-@check_num
-@check_permit
+@check_operand
 def _lt(a, b):
-    return _op_table['lt'][a.tag + b.tag](a, b)
+    return _op_table['<'][a.tag](a, b)
 
-@check_num
-@check_permit
+@check_operand
 def _le(a, b):
-    return _op_table['le'][a.tag + b.tag](a, b)
+    return _op_table['<='][a.tag](a, b)
 
-@check_num
-@check_permit
+@check_operand
 def _gt(a, b):
-    return _op_table['gt'][a.tag + b.tag](a, b)
+    return _op_table['>'][a.tag](a, b)
 
-@check_num
-@check_permit
+@check_operand
 def _ge(a, b):
-    return _op_table['ge'][a.tag + b.tag](a, b)
+    return _op_table['>='][a.tag](a, b)
 
-@check_num
-@check_permit
+@check_operand
 def _eq(a, b):
-    return _op_table['eq'][a.tag + b.tag](a, b)
+    return _op_table['=='][a.tag](a, b)
 
-@check_num
+@check_operand
 def _ne(a, b):
-    return _op_table['ne'][a.tag + b.tag](a, b)
+    return _op_table['!='][a.tag](a, b)
 
 
 # Number types
 class Complex:
     """Real and imaginary parts are guaranteed to be the same type"""
     def __init__(self, real, imag):
+        if real.tag > imag.tag:
+            imag = _convert(imag, real)
+        elif real.tag < imag.tag:
+            real = _convert(real, imag)
         self.real = real
         self.imag = imag
-        if self.real.tag == 'Real':
-            if self.imag.tag != 'Real':
-                self.imag = Real(self.imag.numer / self.imag.denom)
-        else:
-            if self.imag.tag == 'Real':
-                self.real = Real(self.real.numer / self.real.denom)
-        self.tag = 'Complex'
+        self.tag = _COMPLEX
 
     def _conjugate(self):
         return Complex(self.real, -self.imag)
@@ -486,6 +287,9 @@ class Complex:
         return _ne(self, other)
 
     def __str__(self):
+        if Boolean.true(self.imag == Rational(0, 1)):
+            return str(self.real)
+
         if Boolean.true(self.real == Rational(0, 1)):
             if Boolean.true(self.imag == Rational(1, 1)):
                 return 'i'
@@ -504,7 +308,7 @@ class Complex:
 class Real:
     def __init__(self, value):
         self.value = value
-        self.tag = 'Real'
+        self.tag = _REAL
 
     def __add__(self, other):
         return _add(self, other)
@@ -544,7 +348,7 @@ class Rational:
         if self.denom < 0:
             self.numer = -self.numer
             self.denom = -self.denom
-        self.tag = 'Rational'
+        self.tag = _RATIONAL
 
     @staticmethod
     def _gcd(a, b):
