c40b92ab8f7a75617c6b537c494c201a9cb46041
pjhades <pj.hades@gmail.com>
2012-04-13 23:17:00 +0800

    rewrite type mapping, remove suppor for complex numbers like 1/2+4/5i, remove useless encapsulation

---
+++ arith.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+"""
+Arithmetic library. These functions all take as argument a
+python list containing the operands.
+"""
+
+from errors import *
+
+def _check_argc(min_argc):
+    def inner(f):
+        def func(*args):
+            if not min_argc <= len(args):
+                raise SchemeError('', 'wrong argument number')
+            try:
+                return f(*args)
+            except TypeError:
+                raise SchemeError('wrong argument type')
+        return func
+    return inner
+
+
+@_check_argc(0)
+def prim_add(*args):
+    return sum(args)
+    #result = Rational(0, 1)
+    #for arg in args:
+    #    result += arg
+
+    #return result
+
+
+@_check_argc(1)
+def prim_sub(*args):
+    if len(args) == 1:
+        return -args[0]
+    result = args[0]
+    for arg in args[1:]:
+        result -= arg
+    return result
+    #if len(args) == 1:
+    #    return -args[0]
+    #    
+    #result = Rational(0, 1)
+    #result += args[0]
+    #for arg in args[1:]:
+    #    result -= arg
+
+    #return result
+
+
+@_check_argc(0)
+def prim_mul(*args):
+    result = 1
+    for arg in args:
+        result *= arg
+    return result
+    #result = Rational(1, 1)
+    #for arg in args:
+    #    result *= arg
+
+    #return result
+
+@_check_argc(1)
+def prim_div(*args):
+    if len(args) == 1:
+        return 1 / args[0]
+    result = args[0]
+    for arg in args[1:]:
+        result /= arg
+    return result
+    #if len(args) == 1:
+    #    return Rational(1, 1) / args[0]
+    #    
+    #result = Rational(1, 1)
+    #result *= args[0]
+    #for arg in args[1:]:
+    #    result /= arg
+
+    #return result
+
+
+@_check_argc(2)
+def prim_eq(*args):
+    for arg in args[1:]:
+        if arg != args[0]:
+            return False
+    return True
+    #for arg in args[1:]:
+    #    if is_true(arg == args[0]):
+    #        continue
+    #    return Boolean(False)
+    #return Boolean(True)
+
+
+@_check_argc(2)
+def prim_lt(*args):
+    pre = args[0]
+    for arg in args[1:]:
+        if pre >= arg:
+            return False
+        pre = arg
+    return True
+
+@_check_argc(2)
+def prim_le(*args):
+    pre = args[0]
+    for arg in args[1:]:
+        if pre > arg:
+            return False
+        pre = arg
+    return True
+    #now = args[0]
+    #for arg in args[1:]:
+    #    if is_true(now > arg):
+    #        return Boolean(False)
+    #return Boolean(True)
+
+@_check_argc(2)
+def prim_gt(*args):
+    pre = args[0]
+    for arg in args[1:]:
+        if pre <= arg:
+            return False
+        pre = arg
+    return True
+    #now = args[0]
+    #for arg in args[1:]:
+    #    if is_true(now <= arg):
+    #        return Boolean(False)
+    #return Boolean(True)
+
+@_check_argc(2)
+def prim_ge(*args):
+    pre = args[0]
+    for arg in args[1:]:
+        if pre < arg:
+            return False
+        pre = arg
+    return True
+    #now = args[0]
+    #for arg in args[1:]:
+    #    if is_true(now < arg):
+    #        return Boolean(False)
+    #return Boolean(True)
+
---
+++ scmtypes.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+_sym_table = {}
+
+class Symbol(str):
+    """Symbols with the same spell will share the same object."""
+    def __new__(cls, sym):
+        if sym not in _sym_table:
+            return str.__new__(cls, sym)
+        else:
+            return _sym_table[sym]
+    def __init__(self, sym):
+        _sym_table[sym] = self
+    def __eq__(self, other):
+        return super(Symbol, self).__eq__(other)
+    def __cmp__(self, other):
+        return super(Symbol, self).__cmp__(other)
+    def __hash__(self):
+        return super(Symbol, self).__hash__()
+
+
+class Procedure(object):
+    def __init__(self, params, body, env, is_prim=False):
+        """
+        If (lambda args ...), `params' is not a python list, then
+        it's a argument list, `is_var_args' is true. otherwise
+        else `params' is a python list, `is_var_args' is false.
+        """
+        self.params = params
+        self.body = body
+        self.env = env
+        self.is_prim = is_prim 
+        self.is_var_args = not isinstance(params, list)
+
+    def __str__(self):
+        return '[procedure]'
+
+
+def number_query(v):
+    return isinstance(v, int) or isinstance(v, float) or isinstance(v, complex) 
+
+
+def string_query(v):
+    return isinstance(v, str)
+
+
+def symbol_query(v):
+    return isinstance(v, Symbol)
+
+
+def boolean_query(v):
+    return isinstance(v, bool)
+
